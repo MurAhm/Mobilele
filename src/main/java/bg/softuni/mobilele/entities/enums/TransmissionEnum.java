1b9ee752edbcca3fa1c06718096b8add2beeee63
Murad <murad.ahmedov2308@gmail.com>
2021-09-15 12:02:54 -0400

    Mobilele-Part2

--- src/main/java/bg/softuni/mobilele/entities/enums/TransmissionEnum.java
+++
@@ -1,8 +0,0 @@
-package bg.softuni.mobilele.model.entities.enums;
-
-public enum TransmissionEnum {
-
-  MANUAL,
-  AUTOMATIC
-
-}
--- src/main/java/bg/softuni/mobilele/entities/enums/UserRoleEnum.java
+++
@@ -1,6 +0,0 @@
-package bg.softuni.mobilele.model.entities.enums;
-
-public enum UserRoleEnum {
-  ADMIN,
-  USER;
-}
